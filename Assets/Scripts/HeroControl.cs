c61a476cd74616dd38f0e952dd8903b48eeed6d0
maxim218 <maxim.kolotovkin@yandex.ru>
2021-05-30 17:25:58 +0300

    Home work 4

---
+++ Assets/Scripts/HeroControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HeroControl : MonoBehaviour {
+    private int _sumValue = 0;
+
+    private bool _drawBtnFlag = true;
+    
+    private void OnGUI() {
+        // label with score
+        string content = "Count: " + _sumValue;
+        GUI.Box(new Rect(140, 20, 100, 26), content);
+        
+        // reload scene btn
+        if (!_drawBtnFlag) return;
+        const string btnText = "Reload scene";
+        if (!GUI.Button(new Rect(260, 20, 150, 26), btnText)) return;
+        _drawBtnFlag = false;
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    private static IEnumerator LoadSceneAsync() {
+        const string sceneNameString = "SampleScene";
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneNameString);
+        while(!operation.isDone) yield return new WaitForSeconds(1);
+    }
+
+    private void MoneyAdd(int moneyValue) {
+        string message = "Money: " + moneyValue;
+        Debug.Log(message);
+        _sumValue += moneyValue;
+    }
+    
+    public void Catch_2() {
+        const int costs = 2;
+        MoneyAdd(costs);
+    }
+    
+    public void Catch_4() {
+        const int costs = 4;
+        MoneyAdd(costs);
+    }
+}
---
+++ Assets/Scripts/MoneyControl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MoneyControl : MonoBehaviour {
+    [SerializeField] private UnityEvent catchMoneyAction;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        HeroControl heroControl = other.GetComponent<HeroControl>();
+        if (!heroControl) return;
+        catchMoneyAction.Invoke();
+        Destroy(gameObject);
+    }
+}

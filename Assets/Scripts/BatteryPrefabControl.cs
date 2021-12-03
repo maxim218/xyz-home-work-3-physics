02b0af68d82089b0993a34f0e911cd1ae1c62582
m.kolotovkin <m.kolotovkin@corp.mail.ru>
2021-12-03 20:35:49 +0300

    Lighting home task

---
+++ Assets/Scripts/BatteryPrefabControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryPrefabControl : MonoBehaviour
+{
+    [SerializeField] private string heroName = string.Empty;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name.Trim() == heroName)
+        {
+            LampHeroControl script = FindObjectOfType<LampHeroControl>();
+            if (script != null) script.SetEnergyNumber(1);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/LampHeroControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LampHeroControl : MonoBehaviour
+{
+    [SerializeField] private Light2D lightComponent = null;
+    [SerializeField] private bool isLampActive = false;
+    [SerializeField] private float energyNumber = 0;
+
+    public void SetEnergyNumber(float value)
+    {
+        energyNumber = value;
+        lightComponent.intensity = energyNumber;
+    }
+    
+    public void ChangeLampState()
+    {
+        isLampActive = !isLampActive;
+        lightComponent.gameObject.SetActive(isLampActive);
+    }
+
+    private IEnumerator AsyncCounting()
+    {
+        while (true)
+        {
+            const float waitSeconds = 1.0f;
+            yield return new WaitForSeconds(waitSeconds);
+
+            if (isLampActive)
+            {
+                const float deltaEnergy = 0.1f;
+                energyNumber -= deltaEnergy;
+                if (energyNumber < deltaEnergy)
+                    energyNumber = deltaEnergy;
+            }
+
+            lightComponent.intensity = energyNumber;
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine( AsyncCounting() );
+    }
+}

4be788931fff615f4452fc7f2b01f00b76c2faf3
m.kolotovkin <m.kolotovkin@corp.mail.ru>
2021-12-15 02:55:21 +0300

    Pull of objects

--- Assets/Scripts/PistolControl.cs
+++ Assets/Scripts/PistolControl.cs
@@ -1,10 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PistolControl : MonoBehaviour {
    private GameObject _hero = null;
 
+   private Queue<StarControl> _starsQueue = null;
+
+   private void Awake()
+   {
+      _starsQueue = new Queue<StarControl>();
+   }
+
+   public void QueueAdd(StarControl starControl)
+   {
+      _starsQueue.Enqueue(starControl);
+   }
+   
+   private static void ZeroPhysics(GameObject bullet)
+   {
+      Rigidbody2D bulletRigidbody2D = bullet.GetComponent<Rigidbody2D>();
+      bulletRigidbody2D.velocity = Vector2.zero;
+      bulletRigidbody2D.angularVelocity = 0;
+   }
+   
    private void Start() {
       // get hero
       HeroControl heroControl = (HeroControl)FindObjectOfType(typeof(HeroControl));
@@ -29,10 +49,26 @@ public class PistolControl : MonoBehaviour {
 
    [SerializeField] private GameObject bulletPrefab = null;
    
-   private GameObject CreateBullet(Vector3 position) {
-      GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+   public static GameObject InitBullet(GameObject bullet, Vector3 position, bool activeFlag)
+   {
+      ZeroPhysics(bullet);
+      bullet.transform.rotation = Quaternion.Euler(Vector3.zero);
       bullet.transform.position = position;
+      bullet.SetActive(activeFlag);
       return bullet;
+   }
+   
+   private GameObject CreateBullet(Vector3 position) {
+      if (_starsQueue.Count > 0)
+      {
+         StarControl starControl = _starsQueue.Dequeue();
+         return InitBullet(starControl.gameObject, position, true);
+      } 
+      else
+      {
+         GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+         return InitBullet(bullet, position, true);
+      }
    }
 
    [SerializeField] private float forceModule = 1100f;
--- Assets/Scripts/StarControl.cs
+++ Assets/Scripts/StarControl.cs
@@ -1,28 +1,80 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StarControl : MonoBehaviour {
     private GameObject _hero = null;
+
+    private PistolControl _pistolControl = null;
+
+    private IEnumerator _coroutine = null;
+
+    private void StopTiming()
+    {
+        try
+        {
+            if(_coroutine != null)
+            {
+                StopCoroutine(_coroutine);
+                _coroutine = null;
+            }
+        }
+        catch
+        {
+            const string wrnMsg = "Stop timing warning";
+            Debug.LogWarning(wrnMsg);
+        }
+    }
     
-    private void Start() {
-        // get hero
-        HeroControl heroControl = (HeroControl)FindObjectOfType(typeof(HeroControl));
-        _hero = heroControl.gameObject;
-        // wait and destroy
-        StartCoroutine( KillStar() );
+    private PistolControl GetPistolControl()
+    {
+        if (_pistolControl == null) 
+            _pistolControl = FindObjectOfType<PistolControl>();
+        return _pistolControl;
+    }
+
+    private void HeroGet()
+    {
+        if(null == _hero)
+        {
+            HeroControl heroControl = (HeroControl) FindObjectOfType(typeof(HeroControl));
+            _hero = heroControl.gameObject;
+        }
+    }
+    
+    private void Start()
+    {
+        HeroGet();
+    }
+
+    private void OnEnable()
+    {
+        StopTiming();
+        _coroutine = KillStar();
+        StartCoroutine( _coroutine );
     }
 
     private IEnumerator KillStar() {
         const float waitValue = 2.5f;
         yield return new WaitForSeconds(waitValue);
-        Destroy(gameObject);
+        UsePool();
     }
 
+    private void UsePool()
+    {
+        // using pool
+        StopTiming();
+        PistolControl.InitBullet(gameObject, Vector3.zero, false);
+        PistolControl script = GetPistolControl();
+        if (script) 
+            script.QueueAdd(this);
+    }
+    
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject != _hero) return;
         ControlHealth controlHealth = _hero.GetComponent<ControlHealth>();
         controlHealth.AddLives(-1);
-        Destroy(gameObject);
+        UsePool();
     }
 }

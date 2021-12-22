d77856b300836b60ad2083cf0a2daeabe1054337
m.kolotovkin <m.kolotovkin@corp.mail.ru>
2021-12-22 14:29:46 +0300

    Analitics Unity

---
+++ Assets/Scripts/AnalyticsSender.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class AnalyticsSender : MonoBehaviour
+{
+    [SerializeField] private string screenNameStats = string.Empty;
+    
+    private void Start()
+    {
+        Debug.Log("Screen visit: " + screenNameStats);
+        AnalyticsEvent.ScreenVisit(screenNameStats);
+    }
+}
